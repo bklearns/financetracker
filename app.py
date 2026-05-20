53a7d2a011e4b49eb945359d06d99769b578c698
Bianca <biancakhor99@gmail.com>
2026-05-20 15:58:43 +0800

    fix PORT

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import feedparser
 import urllib.request
 import re
+import os
 
 app = Flask(__name__)
 
@@ -44,4 +45,5 @@ def index():
     return render_template("index.html", articles=articles)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
